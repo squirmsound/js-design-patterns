1fa1d5beef366b3586f22423a283da2af81b7c91
squirmsound <squirmsound@gmail.com>
2016-06-22 12:59:46 -0700

    Removed lobal scope by creating an object to hold all chat functionality

--- js/script.js
+++ js/script.js
@@ -1,48 +1,40 @@
-var leadself = 'Me: ',
-		leadcomputer = "PC: ",
-		aSaid= ["This is a Cyber Chat"],
-		msgYes = "Yes, that's a great idea.",
-		msgNo = "No, that must be a mistake.",
-		aSassyStuff = ["Like mold on books, grow myths on history.",
-									"She moved like a poem and smiled like a sphinx.",
-									"As long as we don’t die, this is gonna be one hell of a story.",
-									"She laughed, and the desert sang.",
-									"You’ve got about as much charm as a dead slug."];
+var o = {
+  leadself: 'Me: ',
+	leadcomputer: "PC: ",
+	aSaid: ["This :s a Cyber Chat"],
+	msgYes: "Yes, that's a great idea.",
+	msgNo: "No, that must be a mistake.",
+	aSassyStuff: [
+    "Like mold on books, grow myths on history.",
+    "She moved like a poem and smiled like a sphinx.",
+		"As long as we don’t die, this is gonna be one hell of a story.",
+		"She laughed, and the desert sang.",
+		"You’ve got about as much charm as a dead slug."],
+  talk: function(msg) {
+    this.echo(this.leadself + msg);
+  },
+  replyYesNo: function (msg) {
+    var msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
+    this.echo(this.leadcomputer + msg);
 
-function talk(msg) {
-  echo(leadself + msg);
-}
+  },
+  saySassyStuff: function () {
+    var msg = this.aSassyStuff[ Math.floor(Math.random() * this.aSassyStuff.length)];
+    this.echo(this.leadcomputer + msg);
+  },
+  echo: function (msg) {
+    this.aSaid.push('<div>' + msg + '</div>');
 
-function replyYesNo(msg) {
-  var msg = Math.random() > 0.5 ? msgYes : msgNo;
-  echo(leadcomputer + msg);
+    var aSaidLength = this.aSaid.length,
+      start= Math.max(aSaidLength - 6, 0),
+      out = "";
 
-}
+    for (var i = start; i < aSaidLength; i++) {
+      out += this.aSaid[i];
+    }
 
-function saySassyStuff() {
-  var msg = aSassyStuff[ Math.floor(Math.random() * aSassyStuff.length)];
-  echo(leadcomputer + msg);
-}
+    $('.advert').html(out);
+    $('#talk span').text(msg);
+  },
 
-function echo(msg) {
-  aSaid.push('<div>' + msg + '</div>');
-
-  var aSaidLength = aSaid.length,
-    start= Math.max(aSaidLength - 6, 0),
-    out = "";
-    console.log('start', start);
-
-  for (var i = start; i < aSaidLength; i++) {
-    out += aSaid[i];
-    console.log('i', i);
-  }
-
-  $('.advert').html(out);
-  $('#talk span').text(msg);
-}
-
-
-
-
-
-
+};
