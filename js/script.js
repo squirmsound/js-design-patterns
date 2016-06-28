4f210662fa6bad87002380b1edb0932a5580b832
squirmsound <squirmsound@gmail.com>
2016-06-28 01:17:09 -0700

    Updated module design to Module Reveal

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var chatModule = (function () {
 
   // private
-  var leadself = 'Me: ',
-	leadcomputer = "PC: ",
-	aSaid = ["This :s a Cyber Chat"],
-	msgYes = "Yes, that's a great idea.",
-	msgNo = "No, that must be a mistake.",
-	aSassyStuff = [
+  var _leadself = 'Me: ',
+	_leadcomputer = "PC: ",
+	_aSaid = ["This :s a Cyber Chat"],
+	_msgYes = "Yes, that's a great idea.",
+	_msgNo = "No, that must be a mistake.",
+	_aSassyStuff = [
     "Like mold on books, grow myths on history.",
     "She moved like a poem and smiled like a sphinx.",
 		"As long as we don’t die, this is gonna be one hell of a story.",
@@ -14,33 +14,38 @@ var chatModule = (function () {
 		"You’ve got about as much charm as a dead slug."];
 
   // return API for public access
-  function echo(msg) {
-    aSaid.push('<div>' + msg + '</div>');
+  function _echo(msg) {
+    _aSaid.push('<div>' + msg + '</div>');
 
-    var aSaidLength = aSaid.length,
+    var aSaidLength = _aSaid.length,
       start= Math.max( aSaidLength - 6, 0),
       out = '';
 
     for (var i = start; i < aSaidLength; i++) {
-      out += aSaid[i];
+      out += _aSaid[i];
     }
     $('.advert').html(out);
     $('#talk span').text(msg);
   }
 
+  function talk(msg) {
+    _echo(_leadself + msg);
+  }
+
+  function replyYesNo(msg) {
+    var msg = Math.random() > 0.5 ? _msgYes : _msgNo;
+    _echo(_leadcomputer + msg);
+  }
+
+  function saySassyStuff() {
+    var msg = _aSassyStuff[ Math.floor(Math.random() * _aSassyStuff.length)];
+    _echo(_leadcomputer + msg);
+  }
   // return API for public access
   return {
-    talk: function(msg) {
-      echo(leadself + msg);
-    },
-    replyYesNo: function (msg) {
-      var msg = Math.random() > 0.5 ? msgYes : msgNo;
-      echo(leadcomputer + msg);
-    },
-    saySassyStuff: function () {
-      var msg = aSassyStuff[ Math.floor(Math.random() * aSassyStuff.length)];
-      echo(leadcomputer + msg);
-    },
+    talk: talk,
+    replyYesNo: replyYesNo,
+    saySassyStuff: saySassyStuff,
   };
 })();
 
